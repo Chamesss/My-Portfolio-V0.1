685e78fb28276e9d98d19facf36b067c222f4c24
Chamesss <chamsedin.azouz@gmail.com>
2023-10-10 20:06:43 +0100

    init

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -210,40 +210,6 @@ function Home() {
                                     <p className='typo-from typo-from__box'>Application deployment & scaling</p>
                                 </div>
 
-
-
-                                {/* 
-                                <div className="card">
-                                    <div className="card__side card__side--front">
-                                        <div className="card__picture card__picture--2">
-                                            &nbsp;
-                                        </div>
-                                        <h4 className="card__heading">
-                                            <span className="card__heading-span card__heading-span--2">The Forest Hiker</span>
-                                        </h4>
-                                        <div className="card__details">
-                                            <ul>
-                                                <li>7 day tours</li>
-                                                <li>Up to 40 people</li>
-                                                <li>6 tour guides</li>
-                                                <li>Sleep in provided tents</li>
-                                                <li>Difficulty: medium</li>
-                                            </ul>
-                                        </div>
-
-                                    </div>
-                                    <div className="card__side card__side--back card__side--back-2">
-                                        <div className="card__cta">
-                                            <div className="card__price-box">
-                                                <p className="card__price-only">Only</p>
-                                                <p className="card__price-value">$497</p>
-                                            </div>
-                                            <a href="#popup" className="btn btn--white">Book now!</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                */}
-
                             </div>
                         </div>
                         <div className='line'></div>
