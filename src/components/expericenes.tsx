5bdf88f4325e8dc4c4d32e902425d49c1fdc3d38
Chamesss <chamsedin.azouz@gmail.com>
2023-10-23 04:21:49 +0100

    init

--- src/components/expericenes.tsx
+++ src/components/expericenes.tsx
@@ -1,3 +1,3 @@
 export const experiences = [
-{exp_name: "Fullstack Developer Internship.", place: "Detom", date:"Feb 2023 - May 2023", missions: ["I have successfully created a 'Listing' marketplace platform using React.js, Express.js, and MongoDB.", "This platform offers geocoding integration with OpenStreetMap, providing precise location and interactive map views.", "This project has allowed me to gain in-depth experience in the MERN stack."]},
+{exp_name: "Fullstack Developer Internship.", place: "Detom", date:"Feb 2023 - May 2023", missions: ["I have successfully created a marketplace platform << Annonce >> using React.js, Express.js, and MongoDB.", "This platform offers geocoding integration with OpenStreetMap, providing precise location and interactive map views.", "This project has allowed me to gain in-depth experience in the MERN stack."]},
 ];
